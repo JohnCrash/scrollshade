d767eac226cab4d74bf45a5b7aa5c165323ead6c
john1v7 <udsn2@sina.com>
2019-10-12 17:47:30 +0800

    使用pi zero w构建一个服务器，来控制温室

---
+++ greenpi/src/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//var index = require('./index');
+//var users = require('./users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(config.public));
+app.use(serveStatic('public'));
+
+//app.use('/', index);
+//app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  //res.render('error');
+  res.send(err.toString());
+});
+
+module.exports = app;
