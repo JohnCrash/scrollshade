601862c128995d6ca731e056b40582479852e262
john1v7 <udsn2@sina.com>
2019-10-14 22:34:55 +0800

    1

--- pythongreen/main.py
+++ pythongreen/main.py
@@ -2,18 +2,71 @@
 import time
 import RPi.GPIO as GPIO
 import Adafruit_DHT
+import sqlite3
 
-sensorType = 22
-sensorPin = 18
 
+sensorType = 22 #dht22温湿度传感器
+sensorPin = 18 #dht22读取pin
+jspowerPin = 11 #加速器电源pin
+jsswitchPin = 13 #加湿器开关
+fanPin = 12 #风扇pin
 isfanopen = False
 isjsopen = False
+jsopentime = 0
+last_min = 0
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(jspowerPin,GPIO.OUT)
+GPIO.setup(jsswitchPin,GPIO.OUT)
+GPIO.setup(fanPin,GPIO.OUT)
+
+#每分钟将数据写入到数据库中
+def writeSql60(temp,humi):
+    global last_min
+    t = time.localtime()
+    if last_min!=t.tm_min:
+        try:
+            print('write data')
+            conn = sqlite3.connect('./db/green.db')
+            conn.execute("INSERT INTO data VALUES (datetime(),?,?)",(temp,humi))
+            conn.commit()
+            conn.close()
+        except sqlite3.Error as e:
+            print(e)
+    last_min = t.tm_min
 
 def openJs(b):
-    pass
+    global isjsopen,jsopentime
+    if b:
+        if not isjsopen:
+            #首先需要给加湿器模块供电
+            GPIO.output(jspowerPin,GPIO.HIGH)
+            #打开加湿器需要一个下降波形
+            GPIO.output(jsswitchPin,GPIO.HIGH)
+            time.sleep(0.2)
+            GPIO.output(jsswitchPin,GPIO.LOW)
+            time.sleep(0.2)
+            GPIO.output(jsswitchPin,GPIO.HIGH)
+            isjsopen = True
+            jsopentime = time.time()
+    else:
+        if isjsopen:
+            GPIO.output(jspowerPin,GPIO.LOW)
+            isjsopen = False
+    #加湿操作最长不能超过2小时
+    if isjsopen and time.time()-jsopentime>2*3600:
+        openJs(False)
 
-def openFan(b):  
-    pass
+def openFan(b):
+    global isfanopen
+    if b:
+        if not isfanopen:
+            GPIO.output(fanPin,GPIO.HIGH)
+            isfanopen = True
+    else:
+        if isfanopen:
+            GPIO.output(fanPin,GPIO.LOW)
+            isfanopen = False
 
 while True:
     humi,temp = 0,0
@@ -33,8 +86,10 @@ while True:
         openFan(True)
     else:
         openFan(False)
-    if humi>70:
+    #风扇打开时停止加湿
+    if humi>70 or isfanopen:
         openJs(False)
     elif humi<55 and humi>5:
         openJs(True)
+    writeSql60(temp,humi)
     time.sleep(1)
